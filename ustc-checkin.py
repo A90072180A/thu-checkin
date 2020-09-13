ba3acc53abb98e599265aa9d17a84db6cc947110
A90072180A <38495568+A90072180A@users.noreply.github.com>
2020-09-13 11:43:21 +0800

    Update ustc-checkin.py

--- ustc-checkin.py
+++ ustc-checkin.py
@@ -3,29 +3,15 @@
 import os
 import re
 import requests
-
-# https://stackoverflow.com/a/35504626/5958455
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
-
-
-print("Tsinghua University Daily Health Report")
-
-username = os.environ["USERNAME"]
-password = os.environ["PASSWORD"]
-province = os.environ['PROVINCE']
-city = os.environ["CITY"]
-is_inschool = os.environ.get("IS_INSCHOOL", "2")
-
-# 1: 在校园内, 2: 正常在家
-now_status = "2" if is_inschool == "0" else "1"
-
 
 CAS_LOGIN_URL = "https://passport.ustc.edu.cn/login"
 CAS_RETURN_URL = "https://weixine.ustc.edu.cn/2020/caslogin"
 REPORT_URL = "https://weixine.ustc.edu.cn/2020/daliy_report"
 # Not my fault:                                  ^^
 
+print("Tsinghua University Daily Health Report")
 
 retries = Retry(total=5,
                 backoff_factor=0.5,
@@ -33,36 +19,36 @@ retries = Retry(total=5,
 
 s = requests.Session()
 s.mount("https://", HTTPAdapter(max_retries=retries))
-s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
+s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
 s.get(CAS_LOGIN_URL, params={"service": CAS_RETURN_URL})
 
-data = {
+# Send CAS credentials
+r = s.post("https://passport.ustc.edu.cn/login", data={
     "model": "uplogin.jsp",
     "service": CAS_RETURN_URL,
     "warn": "",
     "showCode": "",
-    "username": username,
-    "password": password,
+    "username": os.environ["USERNAME"],
+    "password": os.environ["PASSWORD"],
     "button": "",
-}
-r = s.post(CAS_LOGIN_URL, data=data)
+})
 
 # Parse the "_token" key out
 x = re.search(r"""<input.*?name="_token".*?>""", r.text).group(0)
 token = re.search(r'value="(\w*)"', x).group(1)
-
-data = {
+r = s.post(REPORT_URL, data={
     "_token": token,
     "now_address": "1",
     "gps_now_address": "",
-    "now_province": province,
+    "now_province": "340000",
     "gps_province": "",
-    "now_city": city,
+    "now_city": "340100",
     "gps_city": "",
     "now_detail": "",
+    "is_inschool": "4",
     "body_condition": "1",
     "body_condition_detail": "",
-    "now_status": now_status,
+    "now_status": "1",
     "now_status_detail": "",
     "has_fever": "0",
     "last_touch_sars": "0",
@@ -76,15 +62,8 @@ data = {
     "last_cross_hubei_detail": "",
     "return_dest": "1",
     "return_dest_detail": "",
-    "other_detail": "\uFFFD",
-    # https://twitter.com/tenderlove/status/722565868719177729
-}
-
-# Set "in_school" only when located in the right city
-if province == "340000" and city == "340100":
-    data["is_inschool"] = is_inschool
-
-r = s.post(REPORT_URL, data=data)
+    "other_detail": "",
+})
 
 # Fail if not 200
 r.raise_for_status()
